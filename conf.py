b0c8c210b26e85635e8f6bd7abecb9fd0b17f94d
huangyisan <huangyisan@qmtv.com>
2018-08-20 15:51:17 +0800

    change theme

--- conf.py
+++ conf.py
@@ -74,7 +74,12 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+#html_theme_options = {
+#    "rightsidebar": "true",
+#    "relbarbgcolor": "black"
+#}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -153,4 +158,3 @@ texinfo_documents = [
      author, 'notebook', 'One line description of project.',
      'Miscellaneous'),
 ]
-
